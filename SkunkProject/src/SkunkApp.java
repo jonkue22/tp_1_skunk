1db04c83908e370ea64a7ed1495404d7cc6c84fb
jonkue22 <jonkue22@gmail.com>
2018-10-19 12:55:02 -0500

    Initial commit for TP-1.2

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -1,19 +1,51 @@
 import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
-public class SkunkApp
+public class SkunkApp implements Reporter
 {
 
 	public static void main(String[] args)
 	{
 		StdOut.println("Welcome to 635 Skunk project!");
-		
+
 		StdOut.println("Play one Turn for player1");
-		
+
 		String player = "player1";
+
+		Turn turn = new Turn();
+
+		while (true)
+		{
+			StdOut.print("Do you want to roll? n or y (ENTER=>y)");
+
+			String response = StdIn.readLine();
+
+			if (!response.equals('n'))
+			{
+				turn.rollAgain();
+				turn.scoreTurn();
+				StdOut.println("You rolled a " + turn.getLastRoll().getDice().getLastRoll());
+				if (turn.ends())
+					break;
+			}
+			else
+			{
+				StdOut.println("You declined to roll.");
+				break;
+			}
+
+		}
 		
-		Turn turn = new Turn(); 
-		
+		int turnScore = turn.getTurnScore();
+		StdOut.println("You scored " + turnScore + " for this turn.");
+
+	}
+
+	@Override
+	public void showMessage(String msg)
+	{
+		StdOut.println(msg);
+
 	}
 
 }
--- SkunkProject/src/Turn.java
+++ SkunkProject/src/Turn.java
@@ -2,5 +2,57 @@ import java.util.ArrayList;
 
 public class Turn
 {
+	private int turnScore;
+	private Roll lastRoll;
+	private ArrayList<Roll> rollSequence;
 	
+	public Turn()
+	{
+		this.turnScore = 0;
+		this.lastRoll = null;
+		this.rollSequence = new ArrayList<>();
+	}
+	
+	public int getTurnScore() {
+		
+		return this.turnScore;
+	}
+
+	public Roll getLastRoll()
+	{
+		return this.lastRoll;
+	}
+
+	public void rollAgain()
+	{
+		this.lastRoll = new Roll();
+		rollSequence.add(this.lastRoll);
+		
+	}
+	
+	public void scoreTurn()
+	{
+		if (getLastRoll().isDoubleSkunk())
+		{
+			turnScore = 0;
+		}
+		else if (getLastRoll().isDeuceSkunk())
+		{
+			turnScore = 0;
+		}
+		else if (getLastRoll().isSingleSkunk())
+		{
+			turnScore = 0;
+		}
+		else
+		{
+			turnScore += lastRoll.getDice().getLastRoll();
+		}
+	}
+
+	public boolean ends()
+	{
+		// TODO Auto-generated method stub
+		return lastRoll.isDeuceSkunk() || lastRoll.isDoubleSkunk() || lastRoll.isSingleSkunk();
+	}
 }
